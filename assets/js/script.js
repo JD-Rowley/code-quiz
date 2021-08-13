ce18f66d104970080e890f801f913298e3de1e39
JD Rowley <jdrowley1989@gmail.com>
2021-08-13 13:05:37 -0600

    add answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,145 +1,167 @@
 var startBtnEl = document.getElementById("start-btn");
+var welcomeBoxEl = document.getElementById("welcome");
 var questionBoxEl = document.getElementById("question-box");
 var questionEl = document.getElementById("question");
 var answerBtnEl = document.getElementById("answer-buttons");
 
-var shuffledQuestions, currentQuestionIndex
+var score = 0;
 
-startBtnEl.addEventListener("click", startQuiz);
+var question, choice, choices, chA, chB, chC, chD;
+var shuffledQuestions, currentQuestionIndex;
 
-function startQuiz() {
-    startBtnEl.classList.add("hide");
-    shuffledQuestions = questionsArrayEl.sort(() => Math.random() - 0.5);
-    currentQuestionIndex = 0
-    questionBoxEl.classList.remove("hide");
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-}
-
-function selectAnswer() {
-
-}
-
-
-
-
-
-
-
-
-// var body = document.body;
-// var questionEl = document.getElementById("question-box");
-// var answerEl = document.getElementById("answer-box");
-// var score = 0;
-
-var questionsArrayEl = [
+var questions = [
     {
         question: "On what world, does the majority of Warcraft take place?",
-        choice: "Nathreza",
-        choice: "Azeroth",
-        choice: "Draenor",
-        choice: "Kalimdor",
-        answer: "Azeroth"
+        a: "Nathreza",
+        b: "Azeroth",
+        c: "Draenor",
+        d: "Kalimdor",
+        answer: "b"
     },{
         question: "Which orc is responsible for the destruction of Draenor?",
-        choice: "Gul'dan",
-        choice: "Thrall",
-        choice: "Ner'zhul",
-        choice: "Illidan",
-        answer: "Ner'zhul"
+        a: "Gul'dan",
+        b: "Thrall",
+        c: "Ner'zhul",
+        d: "Illidan",
+        answer: "c"
     },{
         question: "The guardian, Medivh, can shapeshift into what animal?",
-        choice: "Clefthoof",
-        choice: "Murloc",
-        choice: "Horse",
-        choice: "Raven",
-        answer: "Raven"
+        a: "Clefthoof",
+        b: "Murloc",
+        c: "Horse",
+        d: "Raven",
+        answer: "d"
     },{
         question: "This mage was schooled by the guardian, Medivh.",
-        choice: "Jaina Proudmoore",
-        choice: "Khadgar",
-        choice: "Antonidas",
-        choice: "Krasus",
-        answer: "Khadgar"
+        a: "Jaina Proudmoore",
+        b: "Khadgar",
+        c: "Antonidas",
+        d: "Krasus",
+        answer: "b"
     },{
-        question: "This human kept the orc, Thrall, in servitud as a gladiator until his escape.",
-        choice: "Varian Wrynn",
-        choice: "Anduin Lothar",
-        choice: "Daelin Proudmoore",
-        choice: "Aedelas Blackmoore",
-        answer: "Aedelas Blackmoore"
+        question: "This human kept the orc, Thrall, in servitude as a gladiator until his escape.",
+        a: "Varian Wrynn",
+        b: "Anduin Lothar",
+        c: "Daelin Proudmoore",
+        d: "Aedelas Blackmoore",
+        answer: "d"
     },{
         question: "Which horde warchief destroyed the Vale of Eternal Blossoms, leading to the Siege of Orgrimmar?",
-        choice: "Grommash Hellscream",
-        choice: "Orgrim Doomhammer",
-        choice: "Garrosh Hellscream",
-        choice: "Thrall",
-        answer: "Garrosh Hellscream"
+        a: "Grommash Hellscream",
+        b: "Orgrim Doomhammer",
+        c: "Garrosh Hellscream",
+        d: "Thrall",
+        answer: "c"
     },{
-        question: "Which titan heads the Burning Crusade?",
-        choice: "Sargeras",
-        choice: "Archimonde",
-        choice: "Illidan",
-        choice: "Kil'jaeden",
-        answer: "Sargeras"
+        question: "Which titan leads the Burning Crusade?",
+        a: "Sargeras",
+        b: "Archimonde",
+        c: "Illidan",
+        d: "Kil'jaeden",
+        answer: "a"
     },{
         question: "In what area is the dwarf capital, Ironforge, located?",
-        choice: "Winterspring",
-        choice: "Loch Modan",
-        choice: "Dun Morogh",
-        choice: "Elwynn Forest",
-        answer: "Dun Morogh"
+        a: "Winterspring",
+        b: "Loch Modan",
+        c: "Dun Morogh",
+        d: "Elwynn Forest",
+        answer: "c"
     },{
-        question: "?",
-        choice: "?",
-        choice: "?",
-        choice: "?",
-        choice: "?",
+        question: "9",
+        a: "?",
+        b: "?",
+        c: "?",
+        d: "?",
         answer: "?"
     },{
         question: "10",
-        choice: "?",
-        choice: "?",
-        choice: "?",
-        choice: "?",
+        a: "?",
+        b: "?",
+        c: "?",
+        d: "?",
         answer: "?"
     },
 ];
 
-// var welcomeText = document.createElement("h2");
-// welcomeText.textContent = "Welcome to the Coding Quiz!"
-// welcomeText.setAttribute("style", "margin: auto; text-align: center; font-size: x-large");
-// body.appendChild(welcomeText);
+function startQuiz() {
+    startBtnEl.classList.add("hide");
+    welcomeBoxEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionBoxEl.classList.remove("hide");
+    showQuestion();
+};
 
-// var welcomeParaEl = document.createElement("p");
-// welcomeParaEl.textContent = "Welcome to the coding quiz. Test your knowledge of coding and try to get the high score! You will have 60 seconds to answer 10 coding related questions. Wrong answers subtract time.";
-// welcomeParaEl.setAttribute("style", "margin: 35px auto; text-align: center; font-size: larger");
-// body.appendChild(welcomeParaEl);
-
-// var startBtnEl = document.createElement("button");
-// startBtnEl.className = "submit";
-// startBtnEl.textContent = "Start Quiz!";
-// startBtnEl.setAttribute("style", "margin: auto; font-size: large");
-// startBtnEl.style.position = "relative";
-// startBtnEl.style.left = "48%";
-// startBtnEl.style.right = "48%";
-// body.appendChild(startBtnEl);
-
-// var startQuiz = function() {
-//     for(var i = 0; i < questionsArrayEl.length; i++) {
-//         var questionOptionEl = document.querySelector("#question-box");
-//         questionOptionEl = text.Content = questionsArrayEl[i];
-//         questionOptionEl = setAttribute("value", questionsArrayEl[i]);
-        
-//         questions.appendChild(questionsArrayEl[i]);
-//     };
+// function setNextQuestion() {
+//     showQuestion(shuffledQuestions[currentQuestionIndex]);
 // };
 
-// startBtnEl.addEventListener("click", startQuiz());
+// function createAnswerButtons() {
+//     var answerContainerEl = document.createElement("div");
+//     answerContainerEl.className = "answers";
+
+//     var choiceButton1 = document.createElement("button");
+//     choiceButton1.textContent = choice1;
+//     choiceButton1.className = "answer-buttons";
+
+//     answerContainerEl.appendChild(choiceButton1);
+
+//     var choiceButton2 = document.createElement("button");
+//     choiceButton2.textContent = choice2;
+//     choiceButton2.className = "answer-buttons";
+
+//     answerContainerEl.appendChild(choiceButton2);
+
+//     var choiceButton3 = document.createElement("button");
+//     choiceButton3.textContent = choice3;
+//     choiceButton3.className = "answer-buttons";
+
+//     answerContainerEl.appendChild(choiceButton3);
+
+//     var choiceButton4 = document.createElement("button");
+//     choiceButton4.textContent = choice4;
+//     choiceButton4.className = "answer-buttons";
+
+//     answerContainerEl.appendChild(choiceButton4);
+
+//     return answerContainerEl;
+// }
+
+function showQuestion() {
+    // questionEl.innerText = question.question;
+    if (currentQuestionIndex >= questions.length) {
+        questionEl = "<div> Your score is "+score+" !</div>";
+        return false;
+    }
+
+    question = questions[currentQuestionIndex].question;
+    chA = questions[currentQuestionIndex].a;
+    chB = questions[currentQuestionIndex].b;
+    chC = questions[currentQuestionIndex].c;
+    chD = questions[currentQuestionIndex].d;
+
+    questionEl.innerHTML = "<div>"+question+"</div>";
+
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='A'>"+ chA +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='B'>"+ chB +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='C'>"+ chC +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='D'>"+ chD +"</button>";
+};
+
+function checkAnswer() {
+    choices = document.getElementsByName("choices");
+    for (var i = 0; i < choices.length; i++) {
+        if (choices[i].selected) {
+            choice = choices[i].value;
+        }
+    }
+
+    if (choice === shuffledQuestions[currentQuestionIndex].answer) {
+        score++;
+    }
+
+    showQuestion();
+}
+startBtnEl.addEventListener("click", startQuiz);
+
+answerBtnEl.addEventListener("click", checkAnswer);
