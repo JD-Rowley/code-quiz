25e4acc6ecdce3cd7e328fa7c25b27525948ea53
JD Rowley <jdrowley1989@gmail.com>
2021-08-14 12:15:59 -0600

    add checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var questionBoxEl = document.getElementById("question-box");
 var questionEl = document.getElementById("question");
 var answerBtnEl = document.getElementById("answer-buttons");
 var timerEl = document.getElementById("timer");
+var scoreEl = document.getElementById("score");
 
 var score = 0;
 
@@ -68,116 +69,102 @@ var questions = [
         d: "Elwynn Forest",
         answer: "c"
     },{
-        question: "9",
-        a: "?",
-        b: "?",
-        c: "?",
-        d: "?",
-        answer: "?"
+        question: "As what, was the Black Temple formerly known?",
+        a: "Hope's Light Chapel",
+        b: "Karabor",
+        c: "The Exodar",
+        d: "Naaru",
+        answer: "b"
     },{
-        question: "10",
-        a: "?",
-        b: "?",
-        c: "?",
-        d: "?",
-        answer: "?"
+        question: "Who was the first death knight?",
+        a: "Arthas Menethil",
+        b: "The Lich King",
+        c: "Koltira Deathweaver",
+        d: "Teron Gorefiend",
+        answer: "d"
     },
 ];
 
+// This function will start the quiz
 function startQuiz() {
+    // Hide the start button and welcome text
     startBtnEl.classList.add("hide");
     welcomeBoxEl.classList.add("hide");
+    // Randomize the question order and create index so questions aren't reused
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
+    // Show question box so questions appear onscreen
     questionBoxEl.classList.remove("hide");
+    // Run showQuestion function
     showQuestion();
-    timer();
 };
 
-// function setNextQuestion() {
-//     showQuestion(shuffledQuestions[currentQuestionIndex]);
-// };
-
-// function createAnswerButtons() {
-//     var answerContainerEl = document.createElement("div");
-//     answerContainerEl.className = "answers";
-
-//     var choiceButton1 = document.createElement("button");
-//     choiceButton1.textContent = choice1;
-//     choiceButton1.className = "answer-buttons";
-
-//     answerContainerEl.appendChild(choiceButton1);
-
-//     var choiceButton2 = document.createElement("button");
-//     choiceButton2.textContent = choice2;
-//     choiceButton2.className = "answer-buttons";
-
-//     answerContainerEl.appendChild(choiceButton2);
-
-//     var choiceButton3 = document.createElement("button");
-//     choiceButton3.textContent = choice3;
-//     choiceButton3.className = "answer-buttons";
-
-//     answerContainerEl.appendChild(choiceButton3);
-
-//     var choiceButton4 = document.createElement("button");
-//     choiceButton4.textContent = choice4;
-//     choiceButton4.className = "answer-buttons";
-
-//     answerContainerEl.appendChild(choiceButton4);
-
-//     return answerContainerEl;
-// }
-
+// This function refreshes the screen with new questions and answers
 function showQuestion() {
-    // questionEl.innerText = question.question;
+    // Clear existing question and answers
+    answerBtnEl.innerHTML = "";
+
+    // If there are no questions left, replace question text with user's score
     if (currentQuestionIndex >= questions.length) {
         questionEl = "<div> Your score is "+score+" !</div>";
+        // Break the loop
         return false;
     }
 
+    // Assign values to questions array
     question = shuffledQuestions[currentQuestionIndex].question;
     chA = questions[currentQuestionIndex].a;
     chB = questions[currentQuestionIndex].b;
     chC = questions[currentQuestionIndex].c;
     chD = questions[currentQuestionIndex].d;
 
+    // Assign above question values to question element
     questionEl.innerHTML = "<div>"+question+"</div>";
 
-    answerBtnEl.innerHTML += "<button onclick='checkAnswer()' class='answer-buttons btn' name='choices' value='A'>"+ chA +"</button>";
-    answerBtnEl.innerHTML += "<button onclick='checkAnswer()' class='answer-buttons btn' name='choices' value='B'>"+ chB +"</button>";
-    answerBtnEl.innerHTML += "<button onclick='checkAnswer()' class='answer-buttons btn' name='choices' value='C'>"+ chC +"</button>";
-    answerBtnEl.innerHTML += "<button onclick='checkAnswer()' class='answer-buttons btn' name='choices' value='D'>"+ chD +"</button>";
+    // Generate buttons with answers from questions array
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='a'>"+ chA +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='b'>"+ chB +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='c'>"+ chC +"</button>";
+    answerBtnEl.innerHTML += "<button class='answer-buttons btn' name='choices' value='d'>"+ chD +"</button>";
 };
 
-function checkAnswer() {
-    choices = document.getElementsByName("choices");
-    for (var i = 0; i < choices.length; i++) {
-        if (choices[i].checked) {
-            choice = choices[i].value;
-        }
-    }
+choices = document.getElementsByName("choices");
+answerBtnEl.addEventListener("click", checkAnswer);
 
-    if (choice == questions[currentQuestionIndex].answer) {
+
+// This function will check the answer
+function checkAnswer(e) {
+    // Assign variable to get element more quickly
+    let choice = e.target.value;
+    // Create a for loop to check choices against answers
+
+    //  If choice = answer, then increase score
+    if (choice === questions[currentQuestionIndex].answer) {
         score++;
     } else {
-
-    }
-
+    // If choice =/= answer, decrease time
+        timeLeft--;
+    };
+    // Add 1 to the current question index
     currentQuestionIndex++;
 
+    // Run showQuestion function
     showQuestion();
 }
 
+// Timer function for countdown
 function timer() {
     var timeLeft = 75;
 
-    var timeInterval = setInterval(function() {
+    timerEl.innerHTML = timeLeft;
+
+    var countdown = setInterval(()=> {
         timeLeft--;
-        clearInterval(timeInterval);
+        timerEl.innerHTML = "00:$(timeLeft)";
+        if (timeLeft < 0) {
+        clearInterval(countdown);
+        }
     }, 1000);
 }
 
 startBtnEl.addEventListener("click", startQuiz);
-
-answerBtnEl.addEventListener("click", checkAnswer);
